ced3d2867621d34f9d5d8aeea967839690c441fa
Specinweb <specinweb@gmail.com>
2019-12-22 15:03:51 +0300

    first commit

---
+++ install_ruby.sh
@@ -0,0 +1,10 @@
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+sudo apt install -y gnupg2
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+cd /tmp
+curl -sSL https://get.rvm.io -o rvm.sh
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
