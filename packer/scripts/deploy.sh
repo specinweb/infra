16c1ae9d0f1da4a920926c6416f575b36ed45866
Ivan Pralnikov <specinweb@gmail.com>
2019-12-23 16:15:20 +0300

    Change packer script

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/Artemmkin/reddit.git
-cd reddit && bundle install
+cd ./reddit
+bundle install
 puma -d
-ps aux | grep puma
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -7,5 +7,3 @@ rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
 gem install bundler -V --no-ri --no-rdoc
-ruby -v
-bundle -v
