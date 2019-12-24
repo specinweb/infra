436d48420cb4c6474d97987066df65c724d4015f
Ivan Pralnikov <specinweb@gmail.com>
2019-12-24 09:06:40 +0300

    Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+provider "google" {
+	project = "${var.project}"
+	region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+	name = "reddit-app"
+	machine_type = "n1-standard-1"
+	zone = "europe-west1-b"
+  tags = ["reddit-app"]
+  metadata = {
+    sshKeys = "appuser:${file(var.public_key_path)}"
+  }
+	# определение загрузочного диска
+	boot_disk {
+		initialize_params {
+			image = "${var.disk_image}"
+		}
+	}
+	# определение сетевого интерфейса
+	network_interface {
+		# сеть, к которой присоединить данный интерфейс
+		network = "default"
+		# использовать ephemeral IP для доступа из Интернет
+		access_config {}
+	}
+  connection {
+    type = "ssh"
+    host = "35.187.27.85"
+    user = "appuser"
+    timeout = "3600"
+    agent = true
+    private_key = "${file(var.private_key_path)}"
+  }
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с тегом ...
+  target_tags = ["reddit-app"]
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+project = "devops-1-262914"
+public_key_path = "~/.ssh/appuser.pub"
+private_key_path = "~/.ssh/appuser"
+disk_image = "reddit-base-1577112705"
